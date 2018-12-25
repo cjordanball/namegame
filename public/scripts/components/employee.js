901b67d36962d97b9f35d555cbc77bb850ea7752
Jordan Ball <jordan@cjordanball.info>
2018-12-25 12:29:31 -0500

    add tooltip hint text

--- public/scripts/components/employee.js
+++ public/scripts/components/employee.js
@@ -1,9 +1,21 @@
 const Employee = (employee) => {
+	const containDiv = document.createElement('div');
+	const tipSpan = document.createElement('span');
 	const photo = document.createElement('img');
+
+	containDiv.classList.add('toolTip', 'photoContainer');
+
 	photo.src = employee.headshot.url;
 	photo.dataset.info = JSON.stringify(employee);
 	photo.classList.add('photo');
-	return photo;
+
+	tipSpan.innerText = employee.hint ? employee.hint : 'No hint available.';
+	tipSpan.classList.add('toolTipText');
+
+	containDiv.appendChild(tipSpan);
+	containDiv.appendChild(photo);
+
+	return containDiv;
 };
 
 export default Employee;
